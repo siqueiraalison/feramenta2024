196bba254c0f69ba3d006dc331661500197d7731
siqueiraAlison1 <144942488+siqueiraalison@users.noreply.github.com>
2024-09-30 11:53:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Qual o principal objetivo do Github?',
+    'armazenar seu código, seus arquivos e o histórico de revisão de cada arquivo'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da itália?',
+    'A capital da itália é a Roma'
+)
+
+criaCartao(
+    'Programação',
+    'Qual a Função do Github?',
+    ' é uma plataforma para gerenciar seu código e criar um ambiente de colaboração entre devs'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi itáliano?',
+    'Oi em itáliano é EHI'
+)
